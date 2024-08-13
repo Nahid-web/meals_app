e8eb1a8eb0c2adeb0865073b660b302fbd3c4c86
Nahid-web <nahidamin266@gmailcom>
2024-08-13 14:38:11 +0600

    state management done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/screens/tabs.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const ProviderScope(
+        child: MyApp(),
+      ), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +31,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Nahid amin"),
+      theme: theme,
+      debugShowCheckedModeBanner: false,
+      home: const TabsScreen(),
     );
   }
 }
